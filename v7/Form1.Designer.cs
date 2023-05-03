f93d473a705bf31aca8395da1115b7785127477d
AnasOsta <83444950+AnasOsta@users.noreply.github.com>
2023-05-03 20:36:25 +0300

    Add files via upload

---
+++ v7/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace v7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ad = new System.Windows.Forms.TextBox();
+            this.soyad = new System.Windows.Forms.TextBox();
+            this.bolum = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.id = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.irik = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 164);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(645, 273);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 28);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Constantia", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(14, 11);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Adı";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Constantia", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(315, 14);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Soyadı";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Constantia", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(616, 17);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 29);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Bölüm";
+            // 
+            // ad
+            // 
+            this.ad.Font = new System.Drawing.Font("Constantia", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ad.Location = new System.Drawing.Point(97, 7);
+            this.ad.Margin = new System.Windows.Forms.Padding(4);
+            this.ad.Name = "ad";
+            this.ad.Size = new System.Drawing.Size(210, 36);
+            this.ad.TabIndex = 4;
+            // 
+            // soyad
+            // 
+            this.soyad.Font = new System.Drawing.Font("Constantia", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.soyad.Location = new System.Drawing.Point(398, 14);
+            this.soyad.Margin = new System.Windows.Forms.Padding(4);
+            this.soyad.Name = "soyad";
+            this.soyad.Size = new System.Drawing.Size(210, 36);
+            this.soyad.TabIndex = 5;
+            this.soyad.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // bolum
+            // 
+            this.bolum.Font = new System.Drawing.Font("Constantia", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bolum.Location = new System.Drawing.Point(699, 18);
+            this.bolum.Margin = new System.Windows.Forms.Padding(4);
+            this.bolum.Name = "bolum";
+            this.bolum.Size = new System.Drawing.Size(210, 36);
+            this.bolum.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Constantia", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(19, 104);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 52);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Ekle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Constantia", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(726, 329);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 52);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Sil";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Constantia", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(154, 104);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(127, 52);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Güncelle";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Constantia", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(726, 269);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(127, 52);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Arama";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Constantia", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(726, 389);
+            this.button5.Margin = new System.Windows.Forms.Padding(4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(127, 52);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Göster";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // id
+            // 
+            this.id.Font = new System.Drawing.Font("Constantia", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.id.Location = new System.Drawing.Point(680, 225);
+            this.id.Margin = new System.Windows.Forms.Padding(4);
+            this.id.Name = "id";
+            this.id.Size = new System.Drawing.Size(210, 36);
+            this.id.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Constantia", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(721, 192);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(125, 29);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Öğrenci Id";
+            // 
+            // irik
+            // 
+            this.irik.Font = new System.Drawing.Font("Constantia", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.irik.Location = new System.Drawing.Point(398, 58);
+            this.irik.Margin = new System.Windows.Forms.Padding(4);
+            this.irik.Name = "irik";
+            this.irik.Size = new System.Drawing.Size(210, 36);
+            this.irik.TabIndex = 16;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Constantia", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(315, 57);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 29);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Irık";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(936, 450);
+            this.Controls.Add(this.irik);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.id);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.bolum);
+            this.Controls.Add(this.soyad);
+            this.Controls.Add(this.ad);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox ad;
+        private System.Windows.Forms.TextBox soyad;
+        private System.Windows.Forms.TextBox bolum;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox id;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox irik;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ v7/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Npgsql;
+
+namespace v7
+{
+    public partial class Form1 : Form
+    {
+        //Database start conecctoin
+        private string conString = "Host=localhost;Username=viusal;Password=12345;Database=aaa";
+        private NpgsqlConnection con;
+        private NpgsqlCommand cmd;
+        //Database end conecctoin
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            con = new NpgsqlConnection(conString);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+        }
+        //Shaow button
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //Database start open
+            con.Open();
+
+            NpgsqlCommand command = new NpgsqlCommand("SELECT * FROM ogrenci", con);
+
+            // Execute the query and obtain a result set
+            DataTable dt = new DataTable();
+            dt.Load(command.ExecuteReader());
+
+            dataGridView1.DataSource = dt;
+            con.Close();
+            //Database start close
+        }
+
+        //Insert button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            if (ad.Text == "" || soyad.Text == "" || bolum.Text == "" || irik.Text == "")
+            {
+                MessageBox.Show("Verilerden birini girmeyi unuttun.");
+                return;
+            }
+
+            if (bolum.Text != "bilgisayar")
+            {
+                MessageBox.Show(bolum.Text + ": Bu bölüm bulunmadı");
+                return;
+            }
+
+            con.Open();
+            string sql = "INSERT INTO ogrenci(ad, soyad, bolumno, irik) VALUES(@ad, @soyad, @bolumno, @irik)";
+
+            //insert into ogrenci Start
+            cmd = new NpgsqlCommand(sql, con);
+            cmd.Parameters.AddWithValue("ad", ad.Text);
+            cmd.Parameters.AddWithValue("soyad", soyad.Text);
+            cmd.Parameters.AddWithValue("bolumno", 1);
+            cmd.Parameters.AddWithValue("irik", irik.Text);
+            cmd.Prepare();
+            cmd.ExecuteNonQuery();
+            //insert into ogrenci End
+            MessageBox.Show("Bilgiler başarıyla eklendi");
+            con.Close();
+        }
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.RowIndex >= 0)
+            {
+                ad.Text = dataGridView1.Rows[e.RowIndex].Cells["ad"].Value.ToString();
+                soyad.Text = dataGridView1.Rows[e.RowIndex].Cells["soyad"].Value.ToString();
+                bolum.Text = "bilgisayar";
+                id.Text = dataGridView1.Rows[e.RowIndex].Cells["ogrencino"].Value.ToString();
+                irik.Text = dataGridView1.Rows[e.RowIndex].Cells["irik"].Value.ToString();
+            }
+        }
+
+        //Delet button
+        private void button1_Click(object sender, EventArgs e)
+        {
+                con.Open();
+                string sql = "DELETE FROM ogrenci WHERE ogrencino=" + id.Text;
+                cmd = new NpgsqlCommand(sql, con);
+               
+            if (cmd.ExecuteNonQuery().ToString() == "1")
+                MessageBox.Show("öğrenciyi başarıyla silme");
+            else
+                MessageBox.Show("Bir hata oluşturdu");
+                con.Close();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        //Search button
+        private void button4_Click(object sender, EventArgs e)
+        {
+            con.Open();
+
+            string sql = "SELECT * FROM ogrenci WHERE ogrencino=" + id.Text;
+            cmd = new NpgsqlCommand(sql, con);
+
+            DataTable dt = new DataTable();
+            dt.Load(cmd.ExecuteReader());
+            dataGridView1.DataSource = dt;
+
+            con.Close();
+        }
+        //Update button
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string sql = "";
+
+            con.Open();
+            if (ad.Text != "")
+            {
+                sql = "UPDATE ogrenci SET ad = '" + ad.Text + "' WHERE ogrencino =" + id.Text;
+                cmd = new NpgsqlCommand(sql, con);
+            }
+            if (soyad.Text != "")
+            {
+                sql = "UPDATE ogrenci set soyad = '" + soyad.Text + "' WHERE ogrencino= " + id.Text;
+                cmd = new NpgsqlCommand(sql, con);
+            }
+            if (ad.Text == "" && soyad.Text == "")
+                MessageBox.Show("Giriş bulunamadı");
+            else
+                MessageBox.Show("İşlem başarılı oldu.");
+            cmd.ExecuteNonQuery();
+            con.Close();
+            
+            
+        }
+    }
+}
